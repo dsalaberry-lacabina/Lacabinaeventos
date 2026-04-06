c0b06d58951a3d23cdfbc5de6d824dea7a18100d
Diego Salaberry <dsalaberry@gmail.com>
2026-04-06 17:26:21 -0300

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "La Cabina Eventos",
@@ -8,6 +9,20 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-34SXPRZ7QV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-34SXPRZ7QV');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
